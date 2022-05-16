35024935e077d57ee49d7ff868f4c0fba09ad080
Mert <whoisloyka@gmail.com>
2022-05-17 01:27:01 +0300

    End of  all entity creating but no navigation property

---
+++ src/Loyka.OnMuhasebe.Domain.Shared/Makbuzlar/BelgeDurumu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loyka.OnMuhasebe.Makbuzlar;
+public enum BelgeDurumu
+{
+    Portfoyde=1,
+    Odenecek=2,
+    CiroEdildi=3,
+    TahsisEdildi=4,
+    Odendi=5,
+}
---
+++ src/Loyka.OnMuhasebe.Domain/Subeler/Sube.cs
@@ -0,0 +1,8 @@
+﻿namespace Loyka.OnMuhasebe.Bankalar;
+public class Sube : FullAuditedAggregateRoot<Guid>
+{
+    public string Kod { get; set; }
+    public string Ad { get; set; }
+    public string Aciklama { get; set; }
+    public bool Durum { get; set; }
+}
