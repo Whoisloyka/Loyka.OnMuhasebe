94ab040729fa9572cb2c97726e9f171edd0c6be4
Whoisloyka <whoisloyka@gmail.com>
2022-05-28 13:36:21 +0300

    After ICrudAppService created. Continue from IBankaHesapService.

---
+++ src/Loyka.OnMuhasebe.Application.Contracts/Services/ICrudAppService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Volo.Abp.Application.Services;
+
+namespace Loyka.OnMuhasebe.Services;
+public interface ICrudAppService<TGetOutPutDto, TGetListOutputDto, in TGetListInput, // başına in koyulanlar için: Tanımlama yapılırken in koyulur.
+    in TCreateInput, in TUpdateInput> : 
+    IReadOnlyAppService<TGetListOutputDto, TGetListOutputDto,Guid,TGetListInput>, // GetAsync ve GetListAsync getirir.
+    ICreateAppService<TGetOutPutDto,TCreateInput>, // createInput ile verile alır kaydeder ve geri getirir.
+    IUpdateAppService<TGetOutPutDto,Guid,TUpdateInput> // Guid ve UpdateInputları alır günceller ve geri getirir.
+{
+}
+ public interface ICrudAppService<TGetOutPutDto, TGetListOutputDto, in TGetListInput,
+    in TCreateInput, in TUpdateInput, in TGetCodeInput> :
+    ICrudAppService<TGetOutPutDto, TGetListOutputDto, TGetListInput, TCreateInput, TUpdateInput>,
+    IDeleteAppService<Guid>,
+    ICodeAppService<TGetCodeInput>
